cf3634266b936b539a8c3501eb78cae5e528f393
nitaly31 <geraldine.nitaly@gmail.com>
2021-10-25 19:32:15 -0700

    using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,7 +12,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int swap;
+	int swap, flag = 0;
 
 	if (size <= 1)
 	{
@@ -20,6 +20,7 @@ void bubble_sort(int *array, size_t size)
 	}
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -27,8 +28,11 @@ void bubble_sort(int *array, size_t size)
 				swap = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = swap;
+				flag = 1;
 				print_array(array, size);
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
