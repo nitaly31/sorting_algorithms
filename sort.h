770cb3d98859b5725e6b6401b9b444587735e87d
nitaly31 <geraldine.nitaly@gmail.com>
2021-10-25 20:54:11 -0700

    adding created functions

--- sort.h
+++ sort.h
@@ -29,5 +29,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/*Created functions */
+int partition(int *array, int low, int high, size_t size);
+void sorting(int *array, int low, int high, size_t size);
 
 #endif
